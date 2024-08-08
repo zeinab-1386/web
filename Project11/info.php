0e605cbef3e293cedba21240154896a6d0bcc7f9
zeinab <zizi.golo.1386.zeinab@gmail.com>
2024-08-08 15:53:39 +0330

    Add files via upload

---
+++ Project11/info.php
@@ -0,0 +1,11 @@
+<?php
+
+$Information =array[
+                ("name"=>"Zeinab","lastname"=>"Gharib","age"=>17,"gender"=>false),
+                ("name"=>"Reyhaneh","lastname"=>"Soltani","age"=>16,"gender"=>false),
+                ("name"=>"Honey","lastname"=>"Zarif","age"=>24,"gender"=>false),
+                ("name"=>"Maedeh","lastname"=>"Goharshahi","age"=>24,"gender"=>false),
+                ("name"=>"Mahya","lastname"=>"Hamidi","age"=>16,"gender"=>false),
+                ("name"=>"Aref","lastname"=>"Askari","age"=>16,"gender"=>true),
+                ("name"=>"Mahdi","lastname"=>"Kardani","age"=>20,"gender"=>true)];
+?>
